2c89cf37e565da77549f85f86da32b221950acb1
Trauvin <mateusgalote@gmail.com>
2020-09-23 10:50:36 -0300

    maybe finished

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,7 @@
 class Entry < ApplicationRecord
+    belongs_to :category
+    validates :calories, :proteins, :carbohydrstres, :fats, :meal_type, presence: true
+
     def day
         self.created_at.strftime("%b %e, %Y")
     end
